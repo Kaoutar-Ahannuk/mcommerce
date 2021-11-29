56aef4ca8a7bd2f2cde0be7e153ac55b4fd164fb
Kaoutar-Ahannuk <kaoutarahanuk@gmail.com>
2021-11-29 20:37:04 +0100

    first commit

---
+++ gateway-server/src/test/java/com/mcommerce/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mcommerce.gatewayserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
